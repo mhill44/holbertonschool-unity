25af8dfbfaaad5f1e70af7807c242062a719d38e
mhill44 <3452@holbertonschool.com>
2023-02-02 04:42:38 -0600

    uploading final project

---
+++ 0x00-unity_ar_business_card/Assets/Scripts/social.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void Instagram()
+    {
+        Application.OpenURL("https://www.instagram.com/amsuasari11/");
+    }
+
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com/AmsuAsari");
+    }
+
+    public void github()
+    {
+        Application.OpenURL("https://github.com/mhill44");
+    }
+
+    public void linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/matt-h-8b1065198/");
+    }
+
+    public void youtube()
+    {
+        Application.OpenURL("https://www.youtube.com/user/DementedBeast");
+    }
+}
